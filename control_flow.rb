16de58a6390d7c26d4433302f223039b94a1bafc
Bishnu <bishnuthapako@gmail.com>
2022-07-12 16:40:20 -0400

    commit tree 0fa1904109e5205bb688cb7b352f27c4562ac629

--- control_flow.rb
+++ control_flow.rb
@@ -1,10 +1,31 @@
+require "pry"
+
 def admin_login(username, password)
   # your code here
+  if username == "admin" && password == "12345"
+    return "Access granted"
+  else 
+    # binding.pry
+    return "Access denied"
+  end
+
 end
+# admin_login("admin", "12345")
+admin_login("ADMIN", "12345")
+# admin_login('sudo', '12345')
+# admin_login('admin', 'sudo')
+# admin_login('sudo', 'pls')
+
+
 
 def hows_the_weather(temperature)
   # your code here
+  if temperature < 40
+    return "It's brisk out there!"
+  if temperature 
+  end
 end
+hows_the_weather(35)
 
 def fizzbuzz(num)
   # your code here
